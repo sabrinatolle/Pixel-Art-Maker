ce7090c64e82caae0e0ded0dc26518d4676224d1
Sabrina Tolle <sabrinatolle@outlook.com>
2018-06-13 08:57:08 -0700

    added backgroundColor to add picked color to grid cell

--- designs.js
+++ designs.js
@@ -3,19 +3,23 @@ let color = document.getElementById('colorPicker')
 let table = document.getElementById('pixelCanvas')
 let sizePicker = document.getElementById('sizePicker')
 
-
+let height = document.getElementById(inputHeight);
+let width = document.getElementById(inputWidth);
+    makeGrid(height, width);
 
 
 
 sizePicker.addEventListener('click', (e) => {
     e.preventDefault();
 
-    table.firstElementChild.remove() // deletes everytime we make new grid
-    height = document.getElementById(inputHeight).value;
-    width = document.getElementById(inputWidth).value;
-    makeGrid(height, width)
     
-};
+    let height = document.getElementById(inputHeight).value;
+    let width = document.getElementById(inputWidth).value;
+    table.firstElementChild.remove(); // deletes everytime we make new grid
+   
+    makeGrid(height, width);
+    
+});
 // Select size input
 
 
@@ -29,6 +33,13 @@ function makeGrid(height,width) {
     let row = table.insertRow(i); // for the rows
     for (let k = 0; k <= width; k++){
       let cell = row.insertCell(k); // for the columns
+      // added eventListeners to each cell
+      cell.addEventListener('click', (e) => {
+       // to change the color of each cell clicked
+       cell.style.backgroundColor = color.value;
+        console.log(e);
+      });
+    
     }
   }
 
