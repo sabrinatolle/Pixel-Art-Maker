a9c8836b068a86da31c13f7b6ec97836e4662880
Sabrina Tolle <sabrinatolle@outlook.com>
2018-06-13 08:18:40 -0700

    Made a Grid

--- designs.js
+++ designs.js
@@ -4,12 +4,16 @@ let table = document.getElementById('pixelCanvas')
 let sizePicker = document.getElementById('sizePicker')
 
 
-height = document.getElementById(inputHeight);
-width = document.getElementById(inputWidth);
+
 
 
 sizePicker.addEventListener('click', (e) => {
     e.preventDefault();
+
+    table.firstElementChild.remove() // deletes everytime we make new grid
+    height = document.getElementById(inputHeight).value;
+    width = document.getElementById(inputWidth).value;
+    makeGrid(height, width)
     
 };
 // Select size input
@@ -17,6 +21,17 @@ sizePicker.addEventListener('click', (e) => {
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height,width) {
+  //let row = table.insertRow(0);
+  //let cell = row.insertCell(0);
+
+  for (let i = 0; i <= height; i++){
+    let row = table.insertRow(i); // for the rows
+    for (let k = 0; k <= width; k++){
+      let cell = row.insertCell(k); // for the columns
+    }
+  }
+
+ // console.log(height,width);
   // Your code goes here!
 }
